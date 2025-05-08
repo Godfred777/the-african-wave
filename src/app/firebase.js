b00e24064cd67ea5d9a3d28f014bdeeee150e024
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-05-08 02:30:31 +0000

    feat: add dotenv for environment variable management and update Firebase config

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -2,19 +2,24 @@
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import dotenv from 'dotenv';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+dotenv.config();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmMxC59i6EqeukCe6osD1deuWfgl9yuKw",
-  authDomain: "news-api-f1a79.firebaseapp.com",
-  projectId: "news-api-f1a79",
-  storageBucket: "news-api-f1a79.firebasestorage.app",
-  messagingSenderId: "497185017222",
-  appId: "1:497185017222:web:7cd77d56e81df25258f007",
-  measurementId: "G-8B5BVK52SE"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  //measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
